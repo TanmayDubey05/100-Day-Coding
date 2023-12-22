dbf5f57a33759d598a03be0d8aecfd77c477319d
TANMAY DUBEY <115359989+TanmayDubey05@users.noreply.github.com>
2023-12-22 09:01:33 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (5.18%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                int swap = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=swap;
+            }
+        }
+       for (int[] row : matrix) {
+    int left = 0;
+    int right = row.length - 1;
+    
+    while (left < right) {
+        int temp = row[left];
+        row[left] = row[right];
+        row[right] = temp;
+        
+        left++;
+        right--;
+    }
+}
+
+    }
+    static void swap(int a,int b){
+        int swap = a;
+        a = b;
+        b = swap;
+    }
+}
