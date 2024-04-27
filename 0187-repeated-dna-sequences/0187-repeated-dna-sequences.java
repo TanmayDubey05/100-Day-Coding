8eda4ab08b2a3db90d03d666024d2db909dd9467
TANMAY DUBEY <115359989+TanmayDubey05@users.noreply.github.com>
2024-04-27 20:40:10 +0530

    Time: 21 ms (46.83%), Space: 53.5 MB (39.04%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashMap<String, Integer> map = new HashMap();
+        ArrayList<String> ret = new ArrayList<>();
+
+        for(int i = 0; i<s.length()-9; i++) {
+            String substring = s.substring(i,i+10);
+            if(map.containsKey(substring)){
+                map.replace(substring, map.get(substring)+1);
+            }
+            else{
+                map.put(substring,1);
+            }
+            if (map.get(substring)==2) {
+                ret.add(substring);
+            }
+        }
+        return ret;
+    }
+}
